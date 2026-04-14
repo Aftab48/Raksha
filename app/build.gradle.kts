9f5956dbce4f51f89b9c1260a274d98c3b2806ab
Aftab Alam <mdalam4884@gmail.com>
2026-04-14 20:48:01 +0530

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
 
     // Maps
     implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
     implementation(libs.maps.compose)
     implementation(libs.maps.android.utils)
 
